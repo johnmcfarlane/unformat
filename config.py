83a80a439b308f5ac3f48e3e29d3a5bc38b93006
John McFarlane <fixed-point@john.mcfarlane.name>
2017-08-20 15:29:03 -0700

    initial code commit w. basic functionality

---
+++ config.py
@@ -0,0 +1,47 @@
+from os import path
+from subprocess import check_output, PIPE
+from sys import stderr
+
+from yaml import load, dump
+
+CONFIG_FILENAME = ".clang-format"
+
+
+def make_default_config(command, style):
+    args = [command, "-dump-config"] + ["-style={}".format(style)] if style else []
+    config_buffer = check_output(args, stderr=PIPE).decode('ascii')
+    config = load(config_buffer)
+    return config
+
+
+def make_initial_configs(args):
+    if args.initial:
+        config_filepath = args.initial
+        with open(config_filepath) as config_file:
+            print("Using the provided configuration file, '{}'".format(args.initial), file=stderr)
+            return [load(config_file.read())]
+    elif args.initial is None and args.root:
+        config_filepath = path.join(args.root, CONFIG_FILENAME)
+        with open(config_filepath) as config_file:
+            print("Using the configuration file from the provided root, '{}'".format(args.root))
+            return [load(config_file.read())]
+
+    styles = ["LLVM", "Google", "Chromium", "Mozilla", "WebKit"]
+    print("Using default .clang-format selection: {}".format(styles), file=stderr)
+    return [make_default_config(args.command, style) for style in styles]
+
+
+def present_config(config, args, exiting):
+    config_buffer = dump(config)
+    if args.root:
+        # If output is file, always write out results
+        # because the same copy can be updated with latest and greatest.
+        if not exiting:
+            config_filepath = path.join(args.root, CONFIG_FILENAME)
+            with open(config_filepath, "w") as config_file:
+                config_file.write(config_buffer)
+    else:
+        # If output is stdout, only write out results once on exit
+        # because otherwise, stdout contains multiple copies of config.
+        if exiting:
+            print(config_buffer)
---
+++ measure.py
@@ -0,0 +1,42 @@
+from yaml import dump
+import Levenshtein
+from os import path
+from subprocess import PIPE, Popen, STDOUT
+from sys import stderr
+from tempfile import TemporaryDirectory
+
+
+def get_num_deleted_lines(source_filename, formatted_source):
+    diff_args = ["diff", "--changed-group-format='%<'", "--unchanged-group-format=''", source_filename, "-"]
+    # diff_args = ["wc", "-c", "-"]
+    p = Popen(diff_args, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    diff_output = p.communicate(input=str.encode(formatted_source))[0].decode("utf-8")
+    return diff_output.count('\n')
+
+
+def measure_file(source_filename, workspace_path, command):
+    with open(source_filename) as source_file:
+        source = source_file.read()
+
+    clang_format_args = [command, "-style=file", "-"]
+    p = Popen(clang_format_args, stdout=PIPE, stdin=PIPE, stderr=STDOUT, cwd=workspace_path)
+    formatted_source = p.communicate(input=str.encode(source))[0].decode("utf-8")
+
+    num_deleted_lines = get_num_deleted_lines(source_filename, formatted_source)
+    edit_distance = Levenshtein.distance(source, formatted_source)
+
+    return (num_deleted_lines, edit_distance)
+
+
+def measure(config, source_filenames, args):
+    with TemporaryDirectory() as workspace_path:
+        config_filename = path.join(workspace_path, ".clang-format")
+        with open(config_filename, "wt") as config_file:
+            config_file.write(dump(config))
+
+        scores = [measure_file(source_filename, workspace_path, args.command) for source_filename in source_filenames]
+        num_deleted_lines, edit_distance = [sum(score) for score in zip(*scores)]
+
+        print('.', end='', file=stderr, flush=True)
+
+        return (num_deleted_lines, edit_distance)
